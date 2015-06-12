ef2abd5d362573cc09ac4721d83c468b83648f37
xiayuncheng <xiayuncheng@bilibili.com>
2015-06-12 19:02:28 +0800

    add some explain

--- recyclerviewitemdecoration/src/main/java/com/xyczero/recyclerviewitemdecoration/ItemDecoration.java
+++ recyclerviewitemdecoration/src/main/java/com/xyczero/recyclerviewitemdecoration/ItemDecoration.java
@@ -1,8 +1,7 @@
 package com.xyczero.recyclerviewitemdecoration;
-
-import android.graphics.Rect;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -19,18 +18,31 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
     private int mSpace;
     private int mSpanCount;
     private int mRadixX;
-    private int mCountInFirstLine = 1;
+    private int mItemCountInLastLine;
+    // Record the count of old RV item
+    // Judge whether the data of RV is changed,such as loading next page.
+    // In that case,we need to recalculate the {@value mItemCountInLastLine} and refresh RV.
+    private int mOldItemCount = -1;
 
-    public ItemDecoration(int space, int spaceCount) {
+    public ItemDecoration(int space) {
+        this(space, 1);
+    }
+
+    /**
+     * @param space
+     * @param spanCount spans count of one lane
+     */
+    public ItemDecoration(int space, int spanCount) {
         this.mSpace = space;
-        this.mSpanCount = spaceCount;
-        this.mRadixX = space / spaceCount;
+        this.mSpanCount = spanCount;
+        this.mRadixX = space / spanCount;
     }
 
     @Override
-    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+    public void getItemOffsets(Rect outRect, View view, final RecyclerView parent, RecyclerView.State state) {
         RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) view.getLayoutParams();
-        final int position = params.getViewPosition();
+        final int sumCount = state.getItemCount();
+        final int position = params.getViewLayoutPosition();
         final int spanSize;
         final int index;
 
@@ -38,39 +50,62 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
             GridLayoutManager.LayoutParams gridParams = (GridLayoutManager.LayoutParams) params;
             spanSize = gridParams.getSpanSize();
             index = gridParams.getSpanIndex();
+
+            if ((position == 0 || mOldItemCount != sumCount) && mSpanCount > 1) {
+                int tempPosition = 0;
+                int countInLine = 0;
+                int spanIndex;
+
+                //calculate item count in last line
+                while (tempPosition < sumCount) {
+                    spanIndex = ((GridLayoutManager) parent.getLayoutManager()).getSpanSizeLookup().getSpanIndex(tempPosition++, mSpanCount);
+                    if (spanIndex != 0) {
+                        countInLine++;
+                    } else {
+                        countInLine = 1;
+                    }
+                }
+                mItemCountInLastLine = countInLine;
+
+                if (mOldItemCount != sumCount) {
+                    mOldItemCount = sumCount;
+                    //first load don't need to refresh
+                    if (position != 0) {
+                        parent.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                parent.invalidateItemDecorations();
+                            }
+                        });
+                    }
+                }
+            }
         } else {
             spanSize = 1;
             index = 0;
         }
 
         // invalid value
-        if (spanSize < 1 || index < 0 || spanSize > mSpanCount)
+        if (spanSize < 1 || index < 0 || spanSize > mSpanCount) {
             return;
-
-        if (index == 0 && mSpanCount > 1 && position == 0) {
-            int tempPosition = position;
-            int countInLine = 0;
-            int spanIndex;
-            do {
-                countInLine++;
-                if (tempPosition < state.getItemCount() - 1) {
-                    spanIndex = ((GridLayoutManager) parent.getLayoutManager()).getSpanSizeLookup().getSpanIndex(++tempPosition, mSpanCount);
-                } else {
-                    spanIndex = 0;
-                }
-            } while (spanIndex != 0);
-
-            if (position == 0)
-                mCountInFirstLine = countInLine;
         }
 
         outRect.left = mSpace - mRadixX * index;
         outRect.right = mRadixX + mRadixX * (index + spanSize - 1);
 
-        // set top to all in first lane
-        if (position < mCountInFirstLine) {
-            outRect.top = mSpace;
+        // set bottom to all in last lane
+        if (mSpanCount == 1 && position == sumCount - 1) {
+            outRect.bottom = mSpace;
+        } else if (position >= sumCount - mItemCountInLastLine && position < sumCount) {
+            outRect.bottom = mSpace;
         }
-        outRect.bottom = mSpace;
+
+        //we not unify to set bottom instead of setting top because setting bottom is better.
+        //when remove a item from the recyclerview,we usually use Adapter.notifyItemRemoved(index).
+        //If the item is top or bottom ,it will lead to show incorrectly.
+        //If we unify to set top,we will have two ways to fix it.
+        //One is Adapter.notifyItemRemoved(index-1),the another is adding a footer to the adapter because a footer
+        // usually not been gone.
+        outRect.top = mSpace;
     }
 }
